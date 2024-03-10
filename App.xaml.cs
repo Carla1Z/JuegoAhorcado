5ceb4e45c1e2936cb24b5ce564cf16bc55ba41a8
Carla1Z <carla_sabrina_zarate@hotmail.com>
2024-03-10 16:02:00 -0300

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace JuegoAhorcado;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace JuegoAhorcado;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
