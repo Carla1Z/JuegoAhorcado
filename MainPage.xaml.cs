3e9ac4e9deca8ae3d06034ff19b3bd29b17bb923
Carla1Z <carla_sabrina_zarate@hotmail.com>
2024-03-10 17:23:23 -0300

    seleccionar palabra a adivinar

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,59 @@
-﻿namespace JuegoAhorcado;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 
-public partial class MainPage : ContentPage
+namespace JuegoAhorcado;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
+    #region Fields
+    List<string> words = new List<string>()
+    {
+        "Computadora",
+        "Programación",
+        "Elefante",
+        "Chocolate",
+        "Teléfono",
+        "Avión",
+        "Montaña",
+        "Felicidad",
+        "Escuela",
+        "Biblioteca",
+        "Mariposa",
+        "Guitarra",
+        "Feliz",
+        "Viaje",
+        "Fotografía",
+        "Jugador",
+        "Universo",
+        "Pizza",
+        "Helado"
+    };
+    string answer = "";
+    private string spotlight;
+    List<char> guessed = new List<char>();
+    private List<char> letters = new List<char>();
+    private string message;
+    int mistakes = 0;
+    int maxWrong = 6;
+    private string gameStatus;
+    private string currentImage = "img0.jpg";
+    #endregion
+    public MainPage()
+    {
+        InitializeComponent();
+        PickWord();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    #region Game Engine
+    private void PickWord()
+    {
+        answer =
+             words[new Random().Next(0, words.Count)];
+        Debug.WriteLine(answer);
+    }
+
+    #endregion
+
 
 }
 
