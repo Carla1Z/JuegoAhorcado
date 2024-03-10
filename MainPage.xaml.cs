c8efc120cf8889fab9650bccdfeb860160e5b006
Carla1Z <carla_sabrina_zarate@hotmail.com>
2024-03-10 17:31:13 -0300

    binding palabra a adivinar

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,55 @@ namespace JuegoAhorcado;
 
 public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
+    #region UI Properties
+    public string Spotlight
+    {
+        get => spotlight;
+        set
+        {
+            spotlight = value;
+            OnPropertyChanged();
+        }
+    }
+    public List<char> Letters
+    {
+        get => letters;
+        set
+        {
+            letters = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Message
+    {
+        get => message;
+        set
+        {
+            message = value;
+            OnPropertyChanged();
+        }
+    }
+    public string GameStatus
+    {
+        get => gameStatus;
+        set
+        {
+            gameStatus = value;
+            OnPropertyChanged();
+        }
+    }
+    public string CurrentImage
+    {
+        get => currentImage;
+        set
+        {
+            currentImage = value;
+            OnPropertyChanged();
+        }
+    }
+    #endregion
+
     #region Fields
     List<string> words = new List<string>()
     {
@@ -41,7 +90,9 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
     public MainPage()
     {
         InitializeComponent();
+        BindingContext = this;
         PickWord();
+        CalculateWord(answer, guessed);
     }
 
     #region Game Engine
@@ -50,6 +101,14 @@ public partial class MainPage : ContentPage, INotifyPropertyChanged
         answer =
              words[new Random().Next(0, words.Count)];
         Debug.WriteLine(answer);
+    }
+
+    private void CalculateWord(string answer, List<char> guessed)
+    {
+        var temp =
+             answer.Select(x => (guessed.IndexOf(x) >= 0 ? x : '_'))
+             .ToArray();
+        Spotlight = string.Join(' ', temp);
     }
 
     #endregion
